300e686118f19d9583e9d11f93583c9fd266d9d9
jasonliao1992@163.com <407077499@qq.com>
2019-02-15 14:28:43 +0800

    c

---
+++ examples/main.js
@@ -0,0 +1,34 @@
+/**
+ * Created by aresn on 16/6/20.
+ */
+import 'babel-polyfill';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './app.vue';
+import iView from '../src/index';
+import locale from '../src/locale/lang/zh-CN';
+
+Vue.use(VueRouter);
+Vue.use(iView, {
+    locale
+});
+
+// 开启debug模式
+Vue.config.debug = true;
+
+// 路由配置
+const router = new VueRouter({
+    esModule: false,
+    mode: 'history',
+    routes: [
+        {
+            path: '/affix',
+            component: resolve => require(['./routers/affix.vue'], resolve)
+        }
+    ]
+});
+
+const app = new Vue({
+    router: router,
+    render: h => h(App)
+}).$mount('#app');
